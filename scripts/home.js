3a12227313a05ae5331a5414a2bab3a573f75ca2
Gusales <gussales14@gmail.com>
2024-02-25 10:00:14 -0300

    feat: create card news

---
+++ scripts/home.js
@@ -0,0 +1,16 @@
+import { news } from "./news.js";
+
+let delay = 300
+const contentNews = document.getElementById('content-news')
+news.map(item => {
+  delay += 100
+  contentNews.innerHTML += `
+  <article class="card" style="width: 18rem;" data-aos="fade-up" data-aos-delay="${delay}">
+    <img src="${item.image_url.uri}" class="card-img-top" alt="${item.image_url.description}">
+    <div class="card-body">
+      <h3 class="card-title">${item.title}</h3>
+      <a href="#" class="btn btn-link">Veja mais</a>
+    </div>
+  </article>
+  `
+})
