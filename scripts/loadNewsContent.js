97ea2da6f16d4cef1296df586ebe1b2795e1ca47
Gusales <gussales14@gmail.com>
2024-02-28 13:28:21 -0300

    feat: criar páginas de notícias
    Foi criado a página de notícias únicas, e com js, o conteúdo da página se torna dinâmico

---
+++ scripts/loadNewsContent.js
@@ -0,0 +1,16 @@
+import { news } from "./news.js";
+
+const newsQuery = new URLSearchParams(window.location.search)
+const newsRef = Object.fromEntries(newsQuery.entries())
+
+const [ newsContent ] = news.filter(item => item.id === newsRef.ref)
+console.log(newsContent)
+
+const newsTitleInDocument = document.getElementById('news-title')
+newsTitleInDocument.innerHTML = newsContent.title
+
+const newsCoverInDocument = document.getElementById('news-cover')
+newsCoverInDocument.src = '.' + newsContent.image_url.uri
+
+const newsContentInDocument = document.getElementById('news-content')
+newsContentInDocument.innerHTML = newsContent.content
--- scripts/news.js
+++ scripts/news.js
@@ -1,6 +1,6 @@
 export const news = [
   {
-    id: 1,
+    id: '1',
     title: 'Novo Trailer de "One Piece: Odyssey" Revela Novos Personagens e Enredo Intrigante',
     content: 'Fãs de anime estão frenéticos com o lançamento do novo trailer de "One Piece: Odyssey", o próximo grande título de videogame baseado na icônica série de piratas. O trailer revela novos personagens, incluindo um misterioso antagonista que promete desafiar até mesmo o lendário Monkey D. Luffy. Com visuais impressionantes e uma história original, os fãs mal podem esperar para embarcar nessa jornada épica.',
     image_url: {
@@ -10,7 +10,7 @@ export const news = [
     tags: ['anime', 'game']
   },
   {
-    id: 2,
+    id: '2',
     title: 'Última Temporada de Demon Slayer: Kimetsu no Yaiba Promete Conclusão Épica do Arco da Vila dos Ferreiros',
     content: '',
     image_url: {
@@ -20,7 +20,7 @@ export const news = [
     tags: ['anime']
   },
   {
-    id: 3,
+    id: '3',
     title: '"Elden Ring" Anuncia Expansão Massiva com Novas Regiões, Chefes e História Intrigante',
     content: 'Os jogadores de "Elden Ring" têm motivo para comemorar, pois foi anunciada uma expansão massiva para o jogo aclamado pela crítica. A DLC, intitulada "As Sombras do Abismo", promete levar os jogadores a novas regiões sombrias e perigosas, repletas de inimigos desafiadores e segredos ocultos. Além disso, os fãs podem esperar encontrar chefes épicos nunca antes vistos, cujos desafios testarão até mesmo os jogadores mais habilidosos. Com uma história intrigante que expande ainda mais o rico universo de "Elden Ring", esta expansão promete oferecer horas de jogabilidade emocionante e descobertas surpreendentes. Os fãs mal podem esperar para mergulhar nas sombras do abismo e desvendar seus mistérios.',
     image_url: {
@@ -30,7 +30,7 @@ export const news = [
     tags: ['game']
   },
   {
-    id: 4,
+    id: '4',
     title: 'Lucasfilm Anuncia Nova Série Animada de "Star Wars" Ambientada na Era da Alta República',
     content: 'Os fãs de "Star Wars" têm motivos para comemorar com o anúncio da nova série animada da Lucasfilm, que se passará durante a era da Alta República. Explorando um período pouco explorado da galáxia muito, muito distante, a série promete aventuras emocionantes, novos personagens e revelações surpreendentes sobre a história da Ordem Jedi. Com a reputação da Lucasfilm de criar conteúdo de alta qualidade, as expectativas estão nas alturas para esta nova série.',
     image_url: {
@@ -40,7 +40,7 @@ export const news = [
     tags: ['cinema', 'animação']
   },
   {
-    id: 5,
+    id: '5',
     title: 'Marvel Studios Revela Planos para uma Nova Fase do MCU, Introduzindo Novos Heróis e Vilões',
     content: 'O presidente da Marvel Studios, Kevin Feige, anunciou planos ambiciosos para uma nova fase do Universo Cinematográfico Marvel (MCU), que promete introduzir uma nova gama de heróis e vilões. Com filmes e séries planejados para os próximos anos, os fãs podem esperar por histórias emocionantes, reviravoltas inesperadas e conexões profundas com o vasto universo da Marvel. Com tantas possibilidades em jogo, o futuro do MCU parece mais brilhante do que nunca.',
     image_url: {
@@ -50,7 +50,7 @@ export const news = [
     tags: ['cinema']
   },
   {
-    id: 6,
+    id: '6',
     title: 'Capcom Anuncia Remake de "Resident Evil 4" com Gráficos de Última Geração',
     content: 'Os fãs de horror e jogos de sobrevivência têm motivos para se alegrar com o anúncio da Capcom de um remake de "Resident Evil 4" com gráficos de última geração. Considerado por muitos como um dos melhores jogos da série, o remake promete revitalizar a experiência clássica para uma nova geração de jogadores. Com visuais deslumbrantes, controles aprimorados e novos recursos, "Resident Evil 4" está pronto para aterrorizar os jogadores mais uma vez.',
     image_url: {
@@ -60,7 +60,7 @@ export const news = [
     tags: ['game']
   },
   {
-    id: 7,
+    id: '7',
     title: '"Attack on Titan" Revela Trailer Impactante para a Temporada Final Parte 2',
     content: 'A espera acabou para os fãs de "Attack on Titan" com o lançamento do novo trailer da tão aguardada segunda parte da temporada final. O trailer promete uma conclusão épica para a série, com cenas de ação intensas, revelações chocantes e um confronto final entre os Titãs e a humanidade. Com a série alcançando seu clímax, os fãs estão ansiosos para ver como a história de Eren, Mikasa e Armin chegará a um emocionante desfecho.',
     image_url: {
@@ -70,7 +70,7 @@ export const news = [
     tags: ['anime']
   },
   {
-    id: 8,
+    id: '8',
     title: 'Studio Ghibli Anuncia Novo Filme: "A Jornada do Espírito"',
     content: 'O renomado Studio Ghibli está de volta com o anúncio de seu mais novo filme, intitulado "A Jornada do Espírito". Dirigido pelo aclamado diretor Hayao Miyazaki, o filme promete transportar os espectadores para um mundo de magia, aventura e imaginação. Poucos detalhes foram revelados sobre a trama, mas os fãs podem esperar pela qualidade visual e narrativa excepcionais que se tornaram marca registrada dos filmes do Studio Ghibli. Com uma história envolvente e personagens cativantes, "A Jornada do Espírito" promete encantar audiências de todas as idades.',
     image_url: {
