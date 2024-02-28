810a77330107b5b3d708476e975c759dbdf74279
Gusales <gussales14@gmail.com>
2024-02-28 13:27:29 -0300

    fix: ajustes na animação dos cards de notícias
    Foi ajustado o delay nas animações do AOS

--- scripts/home.js
+++ scripts/home.js
@@ -1,15 +1,15 @@
 import { news } from "./news.js";
 
-let delay = 0
+let count = 0
 const contentNews = document.getElementById('content-news')
 news.map(item => {
-  delay += 200
+  count += 1
   contentNews.innerHTML += `
-  <article class="card" style="width: 18rem;" data-aos="fade-up" data-aos-delay="${delay}">
-    <img src="${item.image_url.uri}" class="card-img-top" alt="${item.image_url.description}">
+  <article class="card" style="width: 18rem;" data-aos="fade-up" data-aos-delay="${count > 4 ? '300' : '400'}">
+    <img src="${item.image_url.uri}" loading="lazy" class="card-img-top" alt="${item.image_url.description}">
     <div class="card-body">
       <h3 class="card-title">${item.title}</h3>
-      <a href="#" class="btn btn-link">Veja mais</a>
+      <a href="./pages/news.html?ref=${item.id}" class="btn btn-link">Veja mais</a>
     </div>
   </article>
   `
